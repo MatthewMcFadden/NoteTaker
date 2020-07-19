ad9ea9fdab18c536d95292030725d9389020e243
Matt M <matt.mcfadden14@gmail.com>
2020-07-19 11:48:48 -0600

    working code after troubleshooting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Note Taker
+Note Taker is an application that can be used to write and save notes. This application uses Express.js and will save and retrieve note data. Users are presented with a landing page with a link to a notes page. When a user clicks on the link to the notes page they are presented with a page with existing saved notes and they are able to add notes as well.
+
+### Deployed at:
+https://note-taker14.herokuapp.com/
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
+const htmlRoutes = require("./routes/htmlRoute");
+const apiRoutes = require("./routes/apiRoute");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-require("./routes/apiRoute.js")(app);
-require("./routes/htmlRoute.js")(app);
-
-app.listen(PORT, () => {
-  console.log(`API server now on PORT: ${PORT}`);
-});
+app.listen(PORT,()=> console.log(`Listening on PORT : ${PORT}`));
